e998e576692b0efae0803e257d74602045f92639
WilliamSavage <williamsavage@hotmail.com>
2016-12-30 22:33:04 +0000

    attempted to make syncronous version to enable bar ticks for progress bar

--- convdir.js
+++ convdir.js
@@ -1,11 +1,21 @@
 #!/usr/bin/env node
-
 var markdownpdf = require("markdown-pdf");
+var ProgressBar = require("ascii-progress")
 var fs = require('fs');
+var path = require('path');
 
+/*PROGRESS BAR*/
+
+var bar = new ProgressBar({
+  schema: ' [:filled.gradient(red,magenta):blank] :current/:total :percent :elapseds :etas'
+});
+
+
+
+/*MD CONVERSION*/
 var currentDir = process.cwd();
 
-var mdDocs = [];
+var mdDocs = '';
 
 fs.readdir(currentDir, (err, files) => {
 
@@ -13,16 +23,27 @@ fs.readdir(currentDir, (err, files) => {
 
   files.forEach((fileName) => {
     if(fileName.split(".")[1] === 'md') {
-      mdDocs.push(fileName);
+
+      mdDocs = (fileName);
+      const rawFsPath = path.join(__dirname, mdDocs);
+      console.log('mdpath: ', rawFsPath);
+
+      var pdfDocs =  "pdfs/" + mdDocs.replace(".md", ".pdf");
+      const pdfTargetFsPath = path.join(__dirname, pdfDocs);
+      console.log('pdfpath: ', pdfTargetFsPath);
+
+      convertMd(mdDocs, pdfDocs)
     }
   })
-
-  var pdfDocs = mdDocs.map( (d) => {
-    return "pdfs/" + d.replace(".md", ".pdf") })
-
-  markdownpdf().from(mdDocs).to(pdfDocs, () => {
-    pdfDocs.forEach((file) => {
-      console.log("\x1b[32m", "Created", file)
-    })
-  });
 });
+
+
+var convertMd = (mdDocs, pdfDocs) => {
+  const rawFsPath = path.join(__dirname, mdDocs);
+  const pdfTargetFsPath = path.join(__dirname, pdfDocs);
+  fs.createReadStream(rawFsPath)
+  .pipe(markdownpdf())
+  .pipe(fs.createWriteStream(pdfTargetFsPath));
+  bar.tick();
+  console.log("\x1b[32m", "Created", pdfDocs);
+};
--- notes.md
+++ notes.md
@@ -1,3 +1,15 @@
+### From multiple paths to multiple paths
+```javascript
+var markdownpdf = require("markdown-pdf")
+
+var mdDocs = ["home.md", "about.md", "contact.md"]
+  , pdfDocs = mdDocs.map(function (d) { return "out/" + d.replace(".md", ".pdf") })
+
+markdownpdf().from(mdDocs).to(pdfDocs, function () {
+  pdfDocs.forEach(function (d) { console.log("Created", d) })
+})
+```
+
 ### Making your new scripts executable
 
 Rather than having to type...
